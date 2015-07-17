7176af505af095ac05ab7062ca285cb73d2a0796
Andy7475 <andylaing5@gmail.com>
2015-07-17 13:58:51 +0100

    created functions for assignment
    checked with a couple of matrices seems to work fine.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The makeCacheMatrix function create an object that can contain a matrix and its inverse.
+## The cacheSolve function can  calculate its inverse, drawing on
+## a cached value of the inverse to save computation time
+## Andrew Laing - Programming Assignment 2 - July 2015
 
-## Write a short comment describing this function
+## makeCacheMatrix creates an object with a list of functions that can contain values
+## such as the matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+  mInverse <- NULL #ensure this value is NULL to begin with
+  #define the matrix using a call to SetMatrix
+  SetMatrix <- function(y){
+    x <<- y
+    mInverse <<-NULL
+  }
+  GetMatrix <- function() x # return the matrix object
+  
+  SetInverse <- function(i){
+    mInverse <<- i # sets the inverse matrix variable
+  }
+  GetInverse <- function() mInverse # gets the inverse matrix variable
+  list(set=SetMatrix,
+       get=GetMatrix,
+       setInverse=SetInverse,
+       getInverse=GetInverse)
 
 }
 
 
-## Write a short comment describing this function
+## This function returns a matrix that is the inverse of x (a matrix), but first checks
+#' to see if the inverse was previously calculated. If it is, it returns it from memory.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse() # get the Inverse matrix(already stored)
+  if(!is.null(inv)) {
+    message("getting cached data") # as the value is stored
+    return(inv)
+  }
+  #if the Inverse is not already stored, then calculate it
+  matrix <- x$get() # get the matrix data
+  inv <- solve(matrix) # calculate the inverse
+  x$setInverse(inv) # Set the inverse in the matrix object
+  inv # return the inverse
 }
